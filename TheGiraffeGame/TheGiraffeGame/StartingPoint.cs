18ce5d1d37261896e3f18ecd58c336214b7d40e4
pepinho24 <p_mil4ev@hotmail.com>
2015-02-11 20:26:10 +0200

    Initial Commit

---
+++ TheGiraffeGame/TheGiraffeGame/StartingPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace TheGiraffeGame
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StartingPoint
+    {
+        static void Main()
+        {
+        }
+    }
+}
