062bbb290c4daeb615ba049e36fcd0b5b21f9a8a
tcholakov <tcholakov2@gmail.com>
2015-02-12 01:55:01 +0200

    Parctle Generation done TODO Movement(not finished)

--- TheGiraffeGame/TheGiraffeGame/FallingRocks.cs
+++ TheGiraffeGame/TheGiraffeGame/FallingRocks.cs
@@ -1,205 +1,205 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace TheGiraffeGame
-{// * Implement the "Falling Rocks" game in the text console. A small dwarf stays at the bottom of the
-    // screen and can move left and right (by the arrows keys). A number of rocks of different sizes and
-    // forms constantly fall down and you need to avoid a crash.
-    // Rocks are the symbols ^, @, *, &, +, %, $, #, !, ., ;, - distributed with appropriate density.
-    // The dwarf is (O). Ensure a constant game speed by Thread.Sleep(150).
-    // Implement collision detection and scoring system.
-
-
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading;
-    using System.Threading.Tasks;
+//namespace TheGiraffeGame
+//{// * Implement the "Falling Rocks" game in the text console. A small dwarf stays at the bottom of the
+//    // screen and can move left and right (by the arrows keys). A number of rocks of different sizes and
+//    // forms constantly fall down and you need to avoid a crash.
+//    // Rocks are the symbols ^, @, *, &, +, %, $, #, !, ., ;, - distributed with appropriate density.
+//    // The dwarf is (O). Ensure a constant game speed by Thread.Sleep(150).
+//    // Implement collision detection and scoring system.
 
 
-    class FallingRocksSolo
-    {
-        static void ResetBuffer()
-        {
-            Console.BufferHeight = Console.WindowHeight = 60;
-            Console.BufferWidth = Console.WindowWidth = 60;
-        }
-
-        static void PrintAtPosition(int x, int y, char symbol, ConsoleColor color)
-        {
-            if (x > 4)
-            {
-                Console.SetCursorPosition(x, y);
-                Console.ForegroundColor = color;
-                Console.Write(symbol);
-            }
-
-        }
-
-        static void PrintStringAtPosition(int x, int y, string text, ConsoleColor color)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.ForegroundColor = color;
-            Console.Write(text);
-        }
-
-        struct Unit
-        {
-            public int x;
-            public int y;
-            public ConsoleColor color;
-            public char symbol;
-        }
-
-        static void Main()
-        {
-            ResetBuffer();
-            Random randomGenerator = new Random();
-            List<Unit> RocksList = new List<Unit>();
-            int livesCount = 1;
-            int score = 0;
-            char[] symbolList = { '^', '*', '&', '+', '%', '$', '#', '!', '.', ';' };
-            int speed = 0;
+//    using System;
+//    using System.Collections.Generic;
+//    using System.Linq;
+//    using System.Text;
+//    using System.Threading;
+//    using System.Threading.Tasks;
 
 
-            // Init Dwarf
-            Unit Dwarf = new Unit();
-            Dwarf.x = 6;
-            Dwarf.y = (Console.WindowHeight / 2) - 1;
-            Dwarf.color = ConsoleColor.White;
-            Dwarf.symbol = '@';
+//    class FallingRocksSolo
+//    {
+//        static void ResetBuffer()
+//        {
+//            Console.BufferHeight = Console.WindowHeight = 60;
+//            Console.BufferWidth = Console.WindowWidth = 60;
+//        }
 
-            while (true)
-            {
-                bool hitted = false;
+//        static void PrintAtPosition(int x, int y, char symbol, ConsoleColor color)
+//        {
+//            if (x > 4)
+//            {
+//                Console.SetCursorPosition(x, y);
+//                Console.ForegroundColor = color;
+//                Console.Write(symbol);
+//            }
 
-                int spawnBuffChance = randomGenerator.Next(0, 100);
+//        }
 
-                if (spawnBuffChance < 10)
-                {
-                    // Spawn buff
-                    Unit newBuff = new Unit();
-                    newBuff.x = Console.WindowWidth;
-                    newBuff.y = randomGenerator.Next(0, Console.WindowHeight - 2);
-                    newBuff.color = ConsoleColor.Red; // We start from blue because black is not Good in our game :)
-                    // newInitRock.color = (ConsoleColor)randomGenerator.Next((int)ConsoleColor.Blue, (int)ConsoleColor.Yellow); // We start from blue because black is not Good in our game :)
-                    newBuff.symbol = '¤'; // TODO: Random
-                    RocksList.Add(newBuff);
-                }
-                else
-                {
-                    // Spawn Rock
-                    Unit newInitRock = new Unit();
-                    newInitRock.x = Console.WindowWidth;
-                    newInitRock.y = randomGenerator.Next(0, Console.WindowHeight - 2);
-                    newInitRock.color = ConsoleColor.Cyan; // We start from blue because black is not Good in our game :)
-                    // newInitRock.color = (ConsoleColor)randomGenerator.Next((int)ConsoleColor.Blue, (int)ConsoleColor.Yellow); // We start from blue because black is not Good in our game :)
-                    newInitRock.symbol = symbolList[randomGenerator.Next(0, 9)]; // TODO: Random
-                    RocksList.Add(newInitRock);
-                }
+//        static void PrintStringAtPosition(int x, int y, string text, ConsoleColor color)
+//        {
+//            Console.SetCursorPosition(x, y);
+//            Console.ForegroundColor = color;
+//            Console.Write(text);
+//        }
 
-                // Move Dwarf
-                if (Console.KeyAvailable)
-                {
-                    ConsoleKeyInfo keyPressed = Console.ReadKey(true);
-                    while (Console.KeyAvailable) { Console.ReadKey(true); }
-                    if (keyPressed.Key == ConsoleKey.DownArrow)
-                    {
-                        if (Dwarf.y > 0)
-                        {
-                            Dwarf.y++;
-                        }
-                    }
-                    if (keyPressed.Key == ConsoleKey.UpArrow)
-                    {
-                        if (Dwarf.y < Console.WindowHeight - 2)
-                        {
-                            Dwarf.y--;
-                        }
-                    }
-                }
+//        struct Unit
+//        {
+//            public int x;
+//            public int y;
+//            public ConsoleColor color;
+//            public char symbol;
+//        }
 
-                // Move Rocks
-                List<Unit> newList = new List<Unit>();
-                for (int i = 0; i < RocksList.Count; i++)
-                {
-                    Unit oldRock = RocksList[i];
-                    Unit NewMovedRock = new Unit();
-                    NewMovedRock.x = oldRock.x - 1;
-                    NewMovedRock.y = oldRock.y;
-                    NewMovedRock.color = oldRock.color;
-                    NewMovedRock.symbol = oldRock.symbol;
+//        static void Main()
+//        {
+//            ResetBuffer();
+//            Random randomGenerator = new Random();
+//            List<Unit> RocksList = new List<Unit>();
+//            int livesCount = 1;
+//            int score = 0;
+//            char[] symbolList = { '^', '*', '&', '+', '%', '$', '#', '!', '.', ';' };
+//            int speed = 0;
 
-                    // Buff Detection
-                    if (NewMovedRock.symbol == '¤' && NewMovedRock.x == Dwarf.x && NewMovedRock.y == Dwarf.y)
-                    {
-                        speed = speed - 50;
-                    }
 
-                    // Collision Detection
-                    if (NewMovedRock.symbol != '¤' && NewMovedRock.x == Dwarf.x && NewMovedRock.y == Dwarf.y)
-                    {
-                        livesCount--;
-                        hitted = true;
-                        speed = 0;
-                        if (livesCount <= 0)
-                        {
-                            PrintStringAtPosition(42, 2, "GAME OVER", ConsoleColor.Red);
-                            PrintStringAtPosition(33, 3, "Press [enter] to continue", ConsoleColor.Red);
-                            Console.ReadLine();
-                        }
-                    }
-                    if (NewMovedRock.x < Console.WindowWidth)
-                    {
-                        newList.Add(NewMovedRock);
-                    }
-                    else
-                    {
-                        score++;
-                    }
-                }
-                RocksList = newList;
+//            // Init Dwarf
+//            Unit Dwarf = new Unit();
+//            Dwarf.x = 6;
+//            Dwarf.y = (Console.WindowHeight / 2) - 1;
+//            Dwarf.color = ConsoleColor.White;
+//            Dwarf.symbol = '@';
 
-                // Clear All
-                Console.Clear();
+//            while (true)
+//            {
+//                bool hitted = false;
 
-                // Draw Dwarf
-                if (hitted)
-                {
-                    PrintAtPosition(Dwarf.x, Dwarf.y, 'X', ConsoleColor.Red);
-                    RocksList.Clear();
-                }
-                else
-                {
-                    PrintAtPosition(Dwarf.x, Dwarf.y, Dwarf.symbol, Dwarf.color);
-                }
+//                int spawnBuffChance = randomGenerator.Next(0, 100);
 
-                // Draw Rocks
-                foreach (Unit rock in RocksList)
-                {
-                    PrintAtPosition(rock.x, rock.y, rock.symbol, rock.color);
-                }
+//                if (spawnBuffChance < 10)
+//                {
+//                    // Spawn buff
+//                    Unit newBuff = new Unit();
+//                    newBuff.x = Console.WindowWidth;
+//                    newBuff.y = randomGenerator.Next(0, Console.WindowHeight - 2);
+//                    newBuff.color = ConsoleColor.Red; // We start from blue because black is not Good in our game :)
+//                    // newInitRock.color = (ConsoleColor)randomGenerator.Next((int)ConsoleColor.Blue, (int)ConsoleColor.Yellow); // We start from blue because black is not Good in our game :)
+//                    newBuff.symbol = '¤'; // TODO: Random
+//                    RocksList.Add(newBuff);
+//                }
+//                else
+//                {
+//                    // Spawn Rock
+//                    Unit newInitRock = new Unit();
+//                    newInitRock.x = Console.WindowWidth;
+//                    newInitRock.y = randomGenerator.Next(0, Console.WindowHeight - 2);
+//                    newInitRock.color = ConsoleColor.Cyan; // We start from blue because black is not Good in our game :)
+//                    // newInitRock.color = (ConsoleColor)randomGenerator.Next((int)ConsoleColor.Blue, (int)ConsoleColor.Yellow); // We start from blue because black is not Good in our game :)
+//                    newInitRock.symbol = symbolList[randomGenerator.Next(0, 9)]; // TODO: Random
+//                    RocksList.Add(newInitRock);
+//                }
 
-                // Draw Score and lives
-                for (int i = 0; i < Console.WindowWidth; i++) // Score Divider
-                {
-                    PrintAtPosition(5, i, '|', ConsoleColor.Gray);
-                }
-                //PrintStringAtPosition(10, 2, "Lives: " + livesCount, ConsoleColor.Green);
-                //PrintStringAtPosition(20, 2, "Score: " + score, ConsoleColor.Green);
-                //PrintStringAtPosition(20, 3, "Speed: " + speed, ConsoleColor.Green);
+//                // Move Dwarf
+//                if (Console.KeyAvailable)
+//                {
+//                    ConsoleKeyInfo keyPressed = Console.ReadKey(true);
+//                    while (Console.KeyAvailable) { Console.ReadKey(true); }
+//                    if (keyPressed.Key == ConsoleKey.DownArrow)
+//                    {
+//                        if (Dwarf.y > 0)
+//                        {
+//                            Dwarf.y++;
+//                        }
+//                    }
+//                    if (keyPressed.Key == ConsoleKey.UpArrow)
+//                    {
+//                        if (Dwarf.y < Console.WindowHeight - 2)
+//                        {
+//                            Dwarf.y--;
+//                        }
+//                    }
+//                }
 
-                // Slow the game down
-                if (speed < 170)
-                {
-                    speed++;
-                }
-                Thread.Sleep(250 - speed);
-            }
-        }
-    }
-}
+//                // Move Rocks
+//                List<Unit> newList = new List<Unit>();
+//                for (int i = 0; i < RocksList.Count; i++)
+//                {
+//                    Unit oldRock = RocksList[i];
+//                    Unit NewMovedRock = new Unit();
+//                    NewMovedRock.x = oldRock.x - 1;
+//                    NewMovedRock.y = oldRock.y;
+//                    NewMovedRock.color = oldRock.color;
+//                    NewMovedRock.symbol = oldRock.symbol;
+
+//                    // Buff Detection
+//                    if (NewMovedRock.symbol == '¤' && NewMovedRock.x == Dwarf.x && NewMovedRock.y == Dwarf.y)
+//                    {
+//                        speed = speed - 50;
+//                    }
+
+//                    // Collision Detection
+//                    if (NewMovedRock.symbol != '¤' && NewMovedRock.x == Dwarf.x && NewMovedRock.y == Dwarf.y)
+//                    {
+//                        livesCount--;
+//                        hitted = true;
+//                        speed = 0;
+//                        if (livesCount <= 0)
+//                        {
+//                            PrintStringAtPosition(42, 2, "GAME OVER", ConsoleColor.Red);
+//                            PrintStringAtPosition(33, 3, "Press [enter] to continue", ConsoleColor.Red);
+//                            Console.ReadLine();
+//                        }
+//                    }
+//                    if (NewMovedRock.x < Console.WindowWidth)
+//                    {
+//                        newList.Add(NewMovedRock);
+//                    }
+//                    else
+//                    {
+//                        score++;
+//                    }
+//                }
+//                RocksList = newList;
+
+//                // Clear All
+//                Console.Clear();
+
+//                // Draw Dwarf
+//                if (hitted)
+//                {
+//                    PrintAtPosition(Dwarf.x, Dwarf.y, 'X', ConsoleColor.Red);
+//                    RocksList.Clear();
+//                }
+//                else
+//                {
+//                    PrintAtPosition(Dwarf.x, Dwarf.y, Dwarf.symbol, Dwarf.color);
+//                }
+
+//                // Draw Rocks
+//                foreach (Unit rock in RocksList)
+//                {
+//                    PrintAtPosition(rock.x, rock.y, rock.symbol, rock.color);
+//                }
+
+//                // Draw Score and lives
+//                for (int i = 0; i < Console.WindowWidth; i++) // Score Divider
+//                {
+//                    PrintAtPosition(5, i, '|', ConsoleColor.Gray);
+//                }
+//                //PrintStringAtPosition(10, 2, "Lives: " + livesCount, ConsoleColor.Green);
+//                //PrintStringAtPosition(20, 2, "Score: " + score, ConsoleColor.Green);
+//                //PrintStringAtPosition(20, 3, "Speed: " + speed, ConsoleColor.Green);
+
+//                // Slow the game down
+//                if (speed < 170)
+//                {
+//                    speed++;
+//                }
+//                Thread.Sleep(250 - speed);
+//            }
+//        }
+//    }
+//}
--- TheGiraffeGame/TheGiraffeGame/StartingPoint.cs
+++ TheGiraffeGame/TheGiraffeGame/StartingPoint.cs
@@ -1,102 +1,128 @@
-﻿/*namespace TheGiraffeGame
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
 
+namespace TheGiraffeGame
+{
     public class StartingPoint
     {
-        public static string[,] Screen;
-
-        public static GiraffesHead GiraffesHeadVar = new GiraffesHead(5, 5);
-
-        static void Main()
-        {
-            Screen = new string[10, 10];
-
-
-            for (int row = 0; row < Screen.GetLength(0); row++)
-            {
-                for (int col = 0; col < Screen.GetLength(1); col++)
-                {
-                    Screen[row, col] = " ";
-                }
-            }
-
-            Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col] = "@";
-
-            string[,] matrix = new string[,]
-                {
-                   { "1", "2"},
-                   { "3", "4"}
-                };
-
-            ConsoleKeyInfo keyinfo;
-
-            do
-            {
-                PrintMatrix(Screen);
-                keyinfo = Console.ReadKey();
-                MoveHead(keyinfo);
-                Console.Clear();
-            }
-            while (keyinfo.Key != ConsoleKey.X);
-        }
 
         private static void MoveHead(ConsoleKeyInfo keyinfo)
         {
+            char emptySpace = ' ';
+            char giraffeHeadChar = '@';
+
             switch (keyinfo.Key)
             {
                 case ConsoleKey.UpArrow:
-                    if (GiraffesHeadVar.Row > 0)
+                    if (GiraffesHeadVar.Col < columns - 1)
                     {
-                        Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col] = " ";
-                        Screen[GiraffesHeadVar.Row - 1, GiraffesHeadVar.Col] = "@";
-                        GiraffesHeadVar.Row = GiraffesHeadVar.Row - 1;
+                        Screen[GiraffesHeadVar.Col, GiraffesHeadVar.Row] = emptySpace;
+                        Screen[GiraffesHeadVar.Col - 1, GiraffesHeadVar.Row] = giraffeHeadChar;
+                        GiraffesHeadVar.Col--;
                     }
                     break;
 
-                case ConsoleKey.DownArrow: 
-                    if (GiraffesHeadVar.Row < Screen.GetLength(0)-1)
+                case ConsoleKey.DownArrow:
+                    if (GiraffesHeadVar.Col > 0)
                     {
-                        Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col] = " ";
-                        Screen[GiraffesHeadVar.Row + 1, GiraffesHeadVar.Col] = "@";
-                        GiraffesHeadVar.Row = GiraffesHeadVar.Row + 1;
-                    } break;
-
-                case ConsoleKey.LeftArrow: if (GiraffesHeadVar.Col > 0)
-                    {
-                        Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col] = " ";
-                        Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col - 1] = "@";
-                        GiraffesHeadVar.Col = GiraffesHeadVar.Col - 1;
-                    } break;
-
-                case ConsoleKey.RightArrow: if (GiraffesHeadVar.Col < Screen.GetLength(1)-1)
-                    {
-                        Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col] = " ";
-                        Screen[GiraffesHeadVar.Row, GiraffesHeadVar.Col + 1] = "@";
-                        GiraffesHeadVar.Col = GiraffesHeadVar.Col + 1;
+                        Screen[GiraffesHeadVar.Col, GiraffesHeadVar.Row] = emptySpace;
+                        Screen[GiraffesHeadVar.Col + 1, GiraffesHeadVar.Row] = giraffeHeadChar;
+                        GiraffesHeadVar.Col++;
                     } break;
                 default:
                     break;
             }
         }
 
-        private static void PrintMatrix(string[,] matrix)
+        private static void particleMove(char[,] screen)
         {
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            char emptySpace = ' ';
+            char giraffeHeadChar = '@';
+            char particleChar = '#';
+
+            Random numGenerator = new Random();
+            int particleY = numGenerator.Next(0, columns - 2);
+            int particleX = screen.GetLength(1);
+
+            for (int col = 0; col < columns; col++)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                char[] buffer = new char[rows];
+
+                for (int i = 0; i < rows-1; i++)
                 {
-                    Console.Write(matrix[row, col]);
+                    if (GiraffesHeadVar.Row == i)
+                    {
+                        screen[GiraffesHeadVar.Col, GiraffesHeadVar.Row] = emptySpace;
+                        screen[GiraffesHeadVar.Col, GiraffesHeadVar.Row+1] = giraffeHeadChar;
+                    }
+                    screen[col,i] = screen[col, i + 1];
+                }
+                if (particleX == rows && particleY == col)
+                {
+                    screen[col, rows - 1] = particleChar;
+                }
+                else {
+                    screen[col, rows - 1] = ' ';
+                }
+                
+            }
+        }
+
+        private static void PrintMatrix(char[,] screen)
+        {
+            for (int col = 0; col < columns; col++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    Console.Write(screen[col, row]);
                 }
                 Console.WriteLine();
             }
         }
 
+        public static char[,] Screen;
+
+        public static GiraffesHead GiraffesHeadVar = new GiraffesHead(20, 5);
+        private static int rows = 60;
+        private static int columns = 20;
+
+        static void Main()
+        {
+            Screen = new Char[columns, rows];
+
+
+            for (int col = 0; col < columns; col++)
+            {
+                for (int row = 0; row < rows; row++)
+                {
+                    Screen[col, row] = ' ';
+                }
+            }
+
+            Screen[GiraffesHeadVar.Col, GiraffesHeadVar.Row] = '@';
+
+            while (true)
+            {
+                Console.Clear();
+                if (Console.KeyAvailable) // true if a key press is available in the input stream
+                {
+
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true); // reads the next key
+                    while (Console.KeyAvailable) // flushes the input stream(the pressed keys are inserted in a queue
+                    {                            // and readKey empties the queue
+                        Console.ReadKey(true);
+                    }
+                    MoveHead(pressedKey);
+                }
+                PrintMatrix(Screen);
+                particleMove(Screen);
+                Thread.Sleep(250);
+            }
+            
+        }
 
     }
 }
-*/
