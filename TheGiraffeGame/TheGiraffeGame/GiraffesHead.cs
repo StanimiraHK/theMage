7c7cb73e7a0641700758b19011933655f4cc0d25
pepinho24 <p_mil4ev@hotmail.com>
2015-03-01 12:52:00 +0200

    Refactored GiraffesHead class

--- TheGiraffeGame/TheGiraffeGame/GiraffesHead.cs
+++ TheGiraffeGame/TheGiraffeGame/GiraffesHead.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheGiraffeGame
+﻿namespace TheGiraffeGame
 {
     public class GiraffesHead
     {
@@ -15,6 +9,7 @@ namespace TheGiraffeGame
         }
 
         public int Row { get; set; }
+
         public int Col { get; set; }
     }
 }
