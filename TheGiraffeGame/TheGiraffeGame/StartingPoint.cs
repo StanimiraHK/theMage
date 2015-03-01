360701378f5dd78f545570bf117a1a1831c32846
StanimiraHK <stanimira.kormeva.4@gmail.com>
2015-03-01 15:44:54 +0200

    Play again option

--- TheGiraffeGame/TheGiraffeGame/StartingPoint.cs
+++ TheGiraffeGame/TheGiraffeGame/StartingPoint.cs
@@ -11,6 +11,7 @@ namespace CursorTest
 {
     class StartingPoint
     {
+        public static string player = string.Empty;
         public static GiraffesHead GiraffesHead;
         private static int rows = 20;
         private static int columns = 60;
@@ -220,7 +221,19 @@ namespace CursorTest
                         stopwatch.Elapsed.Seconds == 0 ? string.Empty : (stopwatch.Elapsed.Hours == 1 ? "1 second" : stopwatch.Elapsed.Seconds + " seconds"));
 
                     SaveScoreToTextFile();
-                    break;
+
+                    //To play again
+                    Console.WriteLine("Hey {0} do you wanna play again. Just press ENTER! Or any other button to close the game.",player);
+                    ConsoleKeyInfo pressedKey = Console.ReadKey(true);
+                    if (pressedKey.Key != ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        isHit = false;
+                        Console.Clear();
+                    }
                 }
 
                 Thread.Sleep(level);
@@ -233,7 +246,7 @@ namespace CursorTest
             //Saving the score to text file ->>>
             Console.WriteLine("Your managed to stay alive for: {0}", timeAlive);
             Console.WriteLine("What is your name, you brave GiraffeWarrior?");
-            string player = Console.ReadLine();
+            player = Console.ReadLine();
             Console.WriteLine(@"Your score has been saved on your TheGiraffeGame\bin\Debug directory - {0}.txt", player);
             Console.WriteLine("Your score has been saved on your TheGiraffeGame\\bin\\Debug directory - Score.txt");
 
